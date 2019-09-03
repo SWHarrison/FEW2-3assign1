7ff20723b95601b5f9229dd808eaae964e0406b5
Sam Harrison <samuel.harrison@students.makeschool.com>
2019-09-02 23:44:04 -0400

    PageContent added

---
+++ src/PageContent.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Project from './Project'
+import './PageContent.css'
+
+function PageContent() {
+  return (
+    <div class ="PageContent">
+      <Project title="Tetris Dots" image="/images/SamHarrison.jpg" link="#" />
+      <Project title="Zombie Server" image="/images/SamHarrison.jpg" link="#" />
+      <Project title="Amazing Colors" image="/images/SamHarrison.jpg" link="#" />
+      <Project title="Flip Toggle" image="/images/SamHarrison.jpg" link="#" />
+      <Project title="121 Second St" image="/images/SamHarrison.jpg" link="#" />
+      <Project title="Slide Shows" image="/images/SamHarrison.jpg" link="#" />
+    </div>
+  )
+}
+
+export default PageContent
